1fff4765eaffc41ffa532cd58ca8bf336a3e7ee7
naxor <naxordeve@gmail.com>
2025-01-29 21:52:28 +0200

    naxor

--- config.js
+++ config.js
@@ -1,1 +1,26 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+dotenv.config({ path: path.resolve(__dirname, `${process.env.NODE_ENV || 'development'}.env`) });
+const toBool = (x) => (x && (x.toLowerCase() === 'true' || x.toLowerCase() === 'on')) || false;
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? "./database.db" : process.env.DATABASE_URL;
 
+const CONFIG = {
+    APP: {
+        SESSION_NAME: process.env.SESSION_NAME || 'Naxor~0NYSjTwJ#fZl6Hvrhnxu369syaYqdZ35YeTSmMkP32L3ByLYYJ7U',
+        BOTNAME: 'AQUASEEK',
+        VERSION: require('./package.json').version,
+        env: process.env.NODE_ENV || 'development',
+        PREFIX: process.env.COMMAND || '.',
+        MODE: toBool(process.env.MODE || "true"),
+        MODS: process.env.MODS || '27686881509,27686567257, 919446072492',
+        OWNER: process.env.ME || '27686881509,27686567257',
+        POST_GET: DATABASE_URL === "./database.db" ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {
+            dialect: "postgres",
+            ssl: true,
+            protocol: "postgres",
+            dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false }, },
+            logging: false,
+        }),
+    },
+};
