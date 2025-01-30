201187f767b3497fba0677358ffc656f88bf62cc
naxor <naxordeve@gmail.com>
2025-01-30 22:30:25 +0200

    naxor

--- plugins/media.js
+++ plugins/media.js
@@ -1,7 +1,30 @@
-const { Sticker } = require('wa-sticker-formatter');
+const { Sticker, createSticker } = require('wa-sticker-formatter');
 const { CreatePlug } = require('../lib/commands');
 const CONFIG = require('../config'); 
 
+CreatePlug({
+    command: 'sticker',
+    category: 'tools',
+    desc: 'Convert an image, GIF, or video into a sticker',
+    execute: async (message, conn, match) => {
+        if (!message.quoted || (!message.quoted.message)) return await message.reply('_Reply to an image, GIF, or video to create a sticker_');
+        const buffer = await message.quoted.download();
+        if (!buffer) return;
+        var pack = CONFIG.APP.PACKNAME;
+        const sticker = new Sticker(buffer, {
+            pack, 
+            type: 'full',             
+            categories: ['🤖', '🎉' '💦'], 
+            quality: 80,              
+            id: 'sticker_cmd',
+            background: '#FFFFFF'     
+        });
+
+        const voidi = await sticker.toBuffer();
+        await conn.sendMessage(message.user, { sticker: voidi });
+    }
+});
+            
 CreatePlug({
     command: 'take',
     category: 'tools',
