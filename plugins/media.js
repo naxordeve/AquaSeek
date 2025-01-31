61b559873a6c729a1ab07c8a21b196721b33734b
naxor <naxordeve@gmail.com>
2025-01-31 11:27:05 +0200

    naxor

--- plugins/media.js
+++ plugins/media.js
@@ -7,15 +7,14 @@ CreatePlug({
     category: 'tools',
     desc: 'Convert an image, GIF, or video into a sticker',
     execute: async (message, conn, match) => {
-        if (!message.quoted || !message.quoted.message) 
-        return await message.reply('_Reply to an image, GIF, or video to create a sticker_');
+        if (!message.quoted || !message.quoted.message) return await message.reply('_Reply to an image, GIF, or video to create a sticker_');
         const buffer = await message.quoted.download();
         if (!buffer) return;
         const pack = CONFIG.APP.STICKER_PACKNAME;
         const sticker = new Sticker(buffer, {
             pack, 
             type: 'full',             
-            categories: ['🤖', '🎉', '💦'], 
+            categories: ['🤖', '🎉', '💦'],  
             quality: 80,              
             id: 'sticker_cmd',
             background: '#FFFFFF'     
@@ -31,11 +30,13 @@ CreatePlug({
     category: 'tools',
     desc: 'Convert an image, GIF, or video into a sticker',
     execute: async (message, conn, match) => {
-        if (!message.quoted || !message.quoted.message) 
-        return await message.reply('_Reply to a sticker, take naxordevi|aquaseek_');
+        if (!message.quoted || !message.quoted.message) return await message.reply('_Reply to a sticker with the format `take packname|author`_');
+        if (!match || !match.includes('|')) {
+            return await message.reply('_You must provide a packname and author in the format `packname|author`_');
+        }
         let [pack, author] = match.split('|').map((v) => v.trim());
-        pack = pack || CONFIG.APP.STICKER_PACKNAME; 
-        author = author || 'NaxorDevi💦'; 
+        if (!pack) pack = CONFIG.APP.STICKER_PACKNAME; 
+        if (!author) author = 'NaxorDevi💦'; 
         const buffer = await message.quoted.download();
         if (!buffer) return;
         const sticker = new Sticker(buffer, {
@@ -51,4 +52,4 @@ CreatePlug({
         await conn.sendMessage(message.user, { sticker: voidi });
     }
 });
-                                       
+    
