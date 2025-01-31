36df83a7234eacc66ff233c18f17561b1865cac2
naxor <naxordeve@gmail.com>
2025-01-31 11:18:05 +0200

    naxor

--- plugins/media.js
+++ plugins/media.js
@@ -1,20 +1,21 @@
 const { Sticker, createSticker } = require('wa-sticker-formatter');
 const { CreatePlug } = require('../lib/commands');
-const CONFIG = require('../config'); 
+const CONFIG = require('../config');
 
 CreatePlug({
     command: 'sticker',
     category: 'tools',
     desc: 'Convert an image, GIF, or video into a sticker',
     execute: async (message, conn, match) => {
-        if (!message.quoted || (!message.quoted.message)) return await message.reply('_Reply to an image, GIF, or video to create a sticker_');
+        if (!message.quoted || !message.quoted.message) 
+        return await message.reply('_Reply to an image, GIF, or video to create a sticker_');
         const buffer = await message.quoted.download();
         if (!buffer) return;
-        var pack = CONFIG.APP.STICKER_PACKNAME;
+        const pack = CONFIG.APP.STICKER_PACKNAME;
         const sticker = new Sticker(buffer, {
             pack, 
             type: 'full',             
-            categories: ['🤖', '🎉' '💦'], 
+            categories: ['🤖', '🎉', '💦'], 
             quality: 80,              
             id: 'sticker_cmd',
             background: '#FFFFFF'     
@@ -24,13 +25,14 @@ CreatePlug({
         await conn.sendMessage(message.user, { sticker: voidi });
     }
 });
-            
+
 CreatePlug({
     command: 'take',
     category: 'tools',
     desc: 'Convert an image, GIF, or video into a sticker',
     execute: async (message, conn, match) => {
-        if (!message.quoted || (!message.quoted.message)) return await message.reply('_Reply to_sticker , take naxordevi|aquaseek_');
+        if (!message.quoted || !message.quoted.message) 
+        return await message.reply('_Reply to a sticker, take naxordevi|aquaseek_');
         let [pack, author] = match.split('|').map((v) => v.trim());
         pack = pack || CONFIG.APP.STICKER_PACKNAME; 
         author = author || 'NaxorDevi💦'; 
@@ -49,4 +51,4 @@ CreatePlug({
         await conn.sendMessage(message.user, { sticker: voidi });
     }
 });
-                                
+                                       
