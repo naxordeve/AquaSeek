d97c51191efebcf7a6197c3de704304673e9247b
naxor <naxordeve@gmail.com>
2025-02-04 10:45:06 +0200

    naxor

---
+++ plugins/DeepSeek.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const { CreatePlug } = require("../lib/commands");
+
+CreatePlug({
+  command: "deepseek",
+  category: "Artficial",
+  desc: "Chat with DeepSeek AI",
+  execute: async (message, conn, match) => {
+    if (!match) return await message.reply("_Please provide a message_");
+    await message.react("⏳");
+    await message.reply("_Thinking..._");
+    let { data } = await axios
+      .post("https://ai.clauodflare.workers.dev/chat", {
+        model: "@cf/deepseek-ai/deepseek-r1-distill-qwen-32b",
+        messages: [{ role: "user", content: match }],
+      })
+      .catch((e) => e.response);
+    const voidi = data?.choices?.[0]?.message?.content || "aquaseek";
+    await conn.sendMessage(message.user, {
+      caption: `\n\n${voidi}`,
+    });
+  },
+});
