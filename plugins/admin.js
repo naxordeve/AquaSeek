12ae06251c2ffb50bd9f09fe8d3fd9adfc3cfabd
naxor <naxordeve@gmail.com>
2025-02-07 13:52:26 +0200

    naxor

--- plugins/admin.js
+++ plugins/admin.js
@@ -5,24 +5,20 @@ CreatePlug({
     category: 'admin',
     desc: 'Block a user',
     execute: async (message, conn) => {
-        if(!message.isFromMe) return;
-        let target = message.quoted ? message.sender: !message.isGroup: false; 
+        if (!message.isFromMe) return;
+        let target = message.quoted 
+            ? message.quoted.sender 
+            : !message.isGroup 
+                ? message.user 
+                : false;
+
         if (!target) return conn.sendMessage(message.user, { text: 'Please reply to a user' });
         await conn.updateBlockStatus(target, 'block');
-        conn.sendMessage(message.user, { text: `_Blocked_ @${target.split('@')[0]}`, mentions: [target] });
+        conn.sendMessage(message.user, { 
+            text: `_Blocked_ @${target.split('@')[0]}`, 
+            mentions: [target] 
+        });
     }
 });
+
           
-CreatePlug({
-    command: 'unblock',
-    category: 'admin',
-    desc: 'Unblock a user',
-    execute: async (message, conn) => {
-        if(!message.isFromMe) return;
-        let target = message.quoted ? message.quoted.participant : message.mentions.length > 0 ? message.mentions[0] : message.sender;
-        if (!target) return conn.sendMessage(message.user, { text: 'Please reply to a user' });
-        await conn.updateBlockStatus(target, 'unblock');
-        conn.sendMessage(message.user, { text: `_Unblocked_ @${target.split('@')[0]}`, mentions: [target] });
-    }
-});
-                                        
