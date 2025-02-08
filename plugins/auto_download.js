0c598de0d17bfe452730af5e63fe2da001f933a4
naxor <naxordeve@gmail.com>
2025-02-08 22:50:10 +0200

    naxor

--- plugins/auto_download.js
+++ plugins/auto_download.js
@@ -1,1 +1,19 @@
+const { CreatePlug } = require("../lib/commands"); 
+const { getBuffer } = require("./functions/funcs"); 
+const { Func } = require("./functions/fbdl"); 
 
+CreatePlug({
+  on: "text",
+  execute: async (message, conn, match) => {
+    await message.react("🔍");
+    if (!match.includes("facebook.com") && !match.includes("fb.watch")) return message.reply('_Provide valid url lol_');
+    const get = await Func(match);
+    if (!get) return;
+    const q = get["720p"] ? "720p (HD)" : "360p (SD)";
+    const voidi = get["720p"] || get["360p"];
+    if (!voidi) return;
+    const toVideo = await getBuffer(voidi);
+    await conn.sendMessage(message.user, { video:{url: toVideo}, caption: `*Quality:* ${q}\n*Source:* ${match}\n\n💦` }, { quoted: message });
+  }
+});
+        
