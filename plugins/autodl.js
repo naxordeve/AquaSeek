0d531572f260a423bf6befda8f14408b8f175678
naxor <naxordeve@gmail.com>
2025-02-10 14:13:07 +0200

    naxor

--- plugins/autodl.js
+++ plugins/autodl.js
@@ -2,6 +2,7 @@ const { CreatePlug } = require("../lib/commands");
 const { Func } = require("./functions/fbdl");
 const { extractUrlFromText } = require("../lib/index.js");
 const { getInstagramReelDownloadURL } = require("./functions/insta_reels"); 
+const { getTikV3 } = require("./functions/tiktokv3"); 
 
 CreatePlug({
   on: "text",
@@ -10,7 +11,9 @@ CreatePlug({
     if (!urls.length) return;
     var igrex = /(?:https?:\/\/)?(?:www\.)?instagram\.com\/[^\s]+/i;
     var fbrex = /(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\/[^\s]+/i;
-    var url = urls[0], isIg = url.match(igrex), isFB = url.match(fbrex);
+    var tikrex = /(?:https?:\/\/)?(?:www\.)?tiktok\.com\/[^\s]+/i;
+    var url = urls[0], isIg = url.match(igrex), isFB = url.match(fbrex), isTik = url.match(tikrex);
+
     if (isIg) {
         await message.reply('_Please wait..._');
         const insta = await getInstagramReelDownloadURL(isIg[0]);
@@ -29,5 +32,15 @@ CreatePlug({
             caption: `*Quality:* ${get["720p"] ? "720p (HD)" : "360p (SD)"}\n*Source:* ${isFB[0]}\n\n💦` 
         }, { quoted: message });
     }
+    if (isTik) {
+        await message.reply('_Please wait..._');
+        const tiktok = await getTikV3(isTik[0]);
+        if (!tiktok || !tiktok.dlv3) return message.reply('_err_');
+        return conn.sendMessage(message.user, { 
+            video: { url: tiktok.dlv3 }, 
+            caption: `*Title:* ${tiktok.title}\n*Source:* ${isTik[0]}\n\n💦` 
+        }, { quoted: message });
+    }
   }
 });
+      
