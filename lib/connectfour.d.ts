a1d8ba3affc9352cfd761359db0e827587f822d3
naxor <naxordeve@gmail.com>
2025-02-10 12:57:49 +0200

    naxor

--- lib/connectfour.d.ts
+++
@@ -1,8 +0,0 @@
-declare class ConnectFour {
-    constructor(player1?: string, player2?: string);
-    getBoard(): string[][];
-    getCurrentPlayer(): string;
-    dropPiece(col: number): { success: boolean; message: string };
-    render(): string;
-}
-export = ConnectFour;
