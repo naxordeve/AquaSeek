73af5cbdc02ae2e7b454f0f647f1ce4c7056c5bc
naxor <naxordeve@gmail.com>
2025-02-10 15:24:16 +0200

    naxor

---
+++ plugins/Lyrics.js
@@ -0,0 +1,18 @@
+var {CreatePlug} = require('../lib/commands');
+var fetch = require('node-fetch');
+
+CreatePlug({
+    command: 'lyrics',
+    category: 'search',
+    desc: 'lyrics based on the query',
+    execute: async (message, conn, match) => {
+        if (!match) return message.reply('Please provide a song name.');
+        const voidi = await fetch(`https://diegoson-naxordeve.hf.space/api/lyrics?q=${match}`);
+        const data = await voidi.json();
+        if (!data?.[0]) return;
+        const song = data[0]; 
+        const Messagei = `🎶 *${song.trackName}* by *${song.artistName}*\n\n${song.plainLyrics}`;
+        message.reply(Messagei);
+    }
+});
+          
