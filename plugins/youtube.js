9dea6128eafcaff7c78ed51d487155ea433bf0e6
naxor <naxordeve@gmail.com>
2025-02-12 21:02:15 +0200

    naxor

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -20,17 +20,17 @@ CreatePlug({
         if (!toAudio.media || !toAudio.media.length) return;
         let naxor_api = toAudio.media.find(m => m.type === 'download')?.link;
         if (!naxor_api) return;
-     //   let cover = await getBuffer(toBuffer.thumbnail);
-        let toMetadata = await AddMetaData(naxor_api, toBuffer.title, toBuffer.author);
+        let cover = await getBuffer(toBuffer.thumbnail);
+        //let toMetadata = await AddMetaData(naxor_api, toBuffer.title, toBuffer.author);
         await conn.sendMessage(message.user, { 
-            audio: { url: toMetadata }, 
+            audio: { url: naxor_api }, 
             mimetype: 'audio/mpeg', 
             fileName: `${toBuffer.title}.mp3`,
             contextInfo: {
                 externalAdReply: {
                     title: toBuffer.title,
                     body: `${toBuffer.duration}`, 
-                    thumbnailUrl: toBuffer.thumbnail, 
+                    thumbnailUrl: cover, 
                     mediaType: 1,
                     renderLargerThumbnail: true,
                     mediaUrl: toBuffer.link,  
