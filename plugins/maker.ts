2537953c94ce218102fa175fb7a4d036b42e68fc
naxor <naxordeve@gmail.com>
2025-02-17 14:40:55 +0200

    naxor

--- plugins/maker.ts
+++ plugins/maker.ts
@@ -1,14 +1,14 @@
-const {CreatePlug} = require('../lib/commands');
+import { CreatePlug } from '../lib/commands';
 
 CreatePlug({
   command: 'neonglitch',
   category: 'tools',
   desc: 'Generate a neon glitch text image',
-  execute: async (message, conn, match) => {
-   await message.react('🤗');
-    var maker = match;
+  execute: async (message: any, conn: any, match: string) => {
+    await message.react('🤗');
+    const maker = match;
     if (!maker) return message.reply("Please provide text to generate a neon glitch image");
-    var api = `https://vapis.my.id/api/neonglitch?q=${maker}`;
+    const api = `https://vapis.my.id/api/neonglitch?q=${maker}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your neon glitch*" });
   },
 });
@@ -17,9 +17,9 @@ CreatePlug({
   command: 'flag3dtext',
   category: 'tools',
   desc: 'Generate a 3D flag text image',
-  execute: async (message, conn, match) => {
+  execute: async (message: any, conn: any, match: string) => {
     if (!match) return message.reply("Please provide text to generate a 3D flag image");
-    var api = `https://vapis.my.id/api/flag3dtext?q=${match}`;
+    const api = `https://vapis.my.id/api/flag3dtext?q=${match}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your 3D flag*" });
   },
 });
@@ -28,9 +28,9 @@ CreatePlug({
   command: 'pixelglitch',
   category: 'tools',
   desc: 'Generate a pixel glitch text image',
-  execute: async (message, conn, match) => {
+  execute: async (message: any, conn: any, match: string) => {
     if (!match) return message.reply("Please provide text to generate a pixel glitch image");
-    var api = `https://vapis.my.id/api/pixelglitch?q=${match}`;
+    const api = `https://vapis.my.id/api/pixelglitch?q=${match}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your pixel glitch*" });
   },
 });
@@ -39,9 +39,9 @@ CreatePlug({
   command: 'logomaker',
   category: 'tools',
   desc: 'Generate a logo with your input text',
-  execute: async (message, conn, match) => {
-    if (!match)  return message.reply("Please provide text to generate a logo");
-    var api = `https://vapis.my.id/api/logomaker?q=${match}`;
+  execute: async (message: any, conn: any, match: string) => {
+    if (!match) return message.reply("Please provide text to generate a logo");
+    const api = `https://vapis.my.id/api/logomaker?q=${match}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your logo*" });
   },
 });
@@ -50,9 +50,9 @@ CreatePlug({
   command: 'writetext',
   category: 'tools',
   desc: 'Generate an image with your input text written on it',
-  execute: async (message, conn, match) => {
-    if (!match) return message.reply("Please provide text to generate an imag");
-    var api = `https://vapis.my.id/api/writetext?q=${match}`;
+  execute: async (message: any, conn: any, match: string) => {
+    if (!match) return message.reply("Please provide text to generate an image");
+    const api = `https://vapis.my.id/api/writetext?q=${match}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your text*" });
   },
 });
@@ -61,10 +61,9 @@ CreatePlug({
   command: 'sandsummer',
   category: 'tools',
   desc: 'Generate a sand summer text image with your input',
-  execute: async (message, conn, match) => {
+  execute: async (message: any, conn: any, match: string) => {
     if (!match) return message.reply("Please provide text to generate a sand summer image");
-    var api = `https://vapis.my.id/api/sandsummer?q=${match}`;
+    const api = `https://vapis.my.id/api/sandsummer?q=${match}`;
     await conn.sendMessage(message.user, { image: { url: api }, caption: "*Here is your sand summer*" });
   },
 });
-      
