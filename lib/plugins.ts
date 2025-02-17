66398d03f9dcf0871692c49552b4421c5d1f8fdb
naxor <naxordeve@gmail.com>
2025-02-17 13:50:26 +0200

    naxor

---
+++ lib/plugins.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function getPlugins(): Promise<string[]> {
+    const commands: string[] = [];
+    const dir: string = path.join(__dirname, '..', 'plugins');
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    console.log('Loading plugins...');
+    fs.readdirSync(dir)
+        .filter((f) => f.endsWith('.js'))
+        .forEach((file) => {
+            try {
+                import(path.join(dir, file)); 
+                commands.push(file);
+            } catch (err) {
+                console.error(`${file}: ${(err as Error).message}`);
+            }
+        });
+
+    console.log(`Plugins loaded ✅\nTotal Plugins: ${commands.length}`);
+    return commands;
+}
+
+export { getPlugins };
