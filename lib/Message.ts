3cd4d6aaea75943eae46c8e55a63f84ae51da906
naxor <naxordeve@gmail.com>
2025-02-17 13:59:22 +0200

    naxor

--- lib/Message.ts
+++ lib/Message.ts
@@ -1,4 +1,4 @@
-const { 
+import { 
     proto, 
     getContentType, 
     jidNormalizedUser, 
@@ -7,10 +7,43 @@ const {
     generateWAMessageFromContent,
     extractMessageContent, 
     normalizeMessageContent 
-} = require('@whiskeysockets/baileys');
+} from '@whiskeysockets/baileys';
 
-const gc_object = {};
-const downloadMedia = async (_msg) => {
+const gc_object: { [key: string]: { admins: string[], expiry: number } } = {};
+
+interface Message {
+    key: { id: string, fromMe: boolean, remoteJid: string, participant?: string };
+    message?: proto.Message;
+    type?: string;
+    user?: string;
+    isFromMe?: boolean;
+    isGroup?: boolean;
+    isBaileys?: boolean;
+    fromBot?: boolean;
+    sender?: string;
+    admins?: string[];
+    isAdmin?: boolean;
+    isBotAdmin?: boolean;
+    quoted?: { 
+        type: string;
+        stanzaId: string;
+        participant: string;
+        message: proto.Message;
+        isFromMe?: boolean;
+        fromBot?: boolean;
+        mtype?: string;
+        text?: string;
+        key: { id: string, fromMe: boolean, remoteJid: string };
+        download: () => Promise<Buffer | null>;
+    };
+    body?: string;
+    reply: (text: string) => void;
+    mentions?: string[];
+    download: () => Promise<Buffer | null>;
+    react: (emoji: string) => Promise<void>;
+}
+
+const downloadMedia = async (_msg: Message): Promise<Buffer | null> => {
     try {
         let type = Object.keys(_msg || {})[0];
         let media = _msg?.[type];
@@ -33,7 +66,7 @@ const downloadMedia = async (_msg) => {
     }
 };
 
-const fetchGroupAdmins = async (jid, conn) => {
+const fetchGroupAdmins = async (jid: string, conn: any): Promise<string[]> => {
     if (gc_object[jid] && gc_object[jid].expiry > Date.now()) {
         return gc_object[jid].admins;
     }
@@ -52,7 +85,7 @@ const fetchGroupAdmins = async (jid, conn) => {
     }
 };
 
-const serialize = async (_msg, conn) => {
+const serialize = async (_msg: Message, conn: any): Promise<Message> => {
     try {
         if (_msg?.key) {
             _msg.id = _msg.key.id;
@@ -66,7 +99,7 @@ const serialize = async (_msg, conn) => {
                 : (_msg.isFromMe
                     ? jidNormalizedUser(conn.user.id)
                     : _msg.user);
-         }
+        }
 
         if (_msg.isGroup) {
             _msg.admins = await fetchGroupAdmins(_msg.user, conn);
@@ -146,7 +179,7 @@ const serialize = async (_msg, conn) => {
                         (_msg.type === 'buttonsResponseMessage' && _msg.message?.[_msg.type]?.selectedButtonId) ||
                         (_msg.type === 'templateButtonReplyMessage' && _msg.message?.[_msg.type]?.selectedId) ||
                         '';
-            _msg.reply = (text) => conn.sendMessage(_msg.user, { text }, { quoted: _msg });
+            _msg.reply = (text: string) => conn.sendMessage(_msg.user, { text }, { quoted: _msg });
             _msg.mentions = (_msg.quoted?.participant ? [_msg.quoted.participant] : []).concat(
                 _msg.message?.[_msg.type]?.contextInfo?.mentionedJid || []
             );
@@ -154,7 +187,7 @@ const serialize = async (_msg, conn) => {
             _msg.download = () => downloadMedia(_msg.message);
         }
         
-        _msg.react = async (emoji) => {
+        _msg.react = async (emoji: string) => {
             try {
                 await conn.sendMessage(_msg.user, { react: { text: emoji, key: _msg.key } });
             } catch (error) {
@@ -169,4 +202,5 @@ const serialize = async (_msg, conn) => {
     }
 };
 
-module.exports = { serialize };
+export { serialize };
+    
