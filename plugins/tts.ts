a0be4ab55128b1aae356747ef9a0a64da6e67c9c
naxor <naxordeve@gmail.com>
2025-02-18 14:14:15 +0200

    naxor

---
+++ plugins/tts.ts
@@ -0,0 +1,23 @@
+import { CreatePlug } from '../lib/commands';
+import gTTS from 'gtts';
+
+CreatePlug({
+    command: 'tts',
+    category: 'convert',
+    desc: 'speech',
+    execute: async (message: any, conn: any, match: string) => {
+        if (!match) return await message.reply('_Need text_');
+        const tts = new gTTS(match, 'en');
+        const path = '/tmp/tts.mp3';
+        tts.save(path, async (err: Error) => {
+            if (err) {
+                console.error(err);
+                return;
+            }try {
+                await conn.sendMessage(message.user, { audio: { url: path }, mimetype: 'audio/mp4' });
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    }
+});
