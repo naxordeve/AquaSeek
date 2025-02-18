5a9d95423d4d76d182450e3f859fdedb0536328e
naxor <naxordeve@gmail.com>
2025-02-18 14:54:41 +0200

    naxor

--- lib/commands.ts
+++ lib/commands.ts
@@ -1,15 +1,16 @@
+type ExecuteFunction = (message: any, conn: any, match: string) => Promise<void>;
 type Command = {
-    command: string;
-    category: string;
-    desc: string;
-    execute: Function;
-    on?: string;
+  command: string;
+  category: string;
+  desc: string;
+  execute: ExecuteFunction; 
+  on?: string;
 };
 
 const commands: Command[] = [];
 function CreatePlug({ command, category, desc, execute, on }: Command): void {
-    const commandData: Command = { command, category, desc, execute, on };
-    commands.push(commandData);
+  const commandData: Command = { command, category, desc, execute, on };
+  commands.push(commandData);
 }
 
 export { commands, CreatePlug };
