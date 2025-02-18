e229ff3ed519e07d5ac3fff133910a6189664766
naxor <naxordeve@gmail.com>
2025-02-18 20:14:20 +0200

    Update

--- plugins/functions/search.ts
+++ plugins/functions/search.ts
@@ -1,13 +1,43 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+
+interface NpmPackage {
+  packageName: string;
+  latestVersion: string;
+  initialVersion: string;
+  updatesCount: number;
+  latestDependenciesCount: number;
+  initialDependenciesCount: number;
+  firstPublished: string;
+  lastUpdated: string;
+}
+
+interface GitHubUser {
+  username: string;
+  nickname: string;
+  bio: string;
+  profilePicture: string;
+  profileUrl: string;
+  userType: string;
+  isAdmin: boolean;
+  company: string;
+  blog: string;
+  location: string;
+  publicRepos: number;
+  publicGists: number;
+  followers: number;
+  following: number;
+  accountCreated: string;
+  lastUpdated: string;
+}
 
 const APIS = {
-  npmSearch: async (packageName) => {
+  npmSearch: async (packageName: string): Promise<NpmPackage | null> => {
     const _api = `https://api.siputzx.my.id/api/stalk/npm?packageName=${packageName}`;
     try {
       const res = await fetch(_api);
       const result = await res.json();
       if (!result.status) {
-      throw new Error("err");
+        throw new Error("Error fetching npm package data");
       }
       const {
         name,
@@ -29,19 +59,19 @@ const APIS = {
         firstPublished: new Date(publishTime).toLocaleString(),
         lastUpdated: new Date(latestPublishTime).toLocaleString(),
       };
-    } catch (error) {
+    } catch (error: any) {
       console.error(error.message);
       return null;
     }
   },
 
-  GIT: async (githubUser) => {
+  GIT: async (githubUser: string): Promise<GitHubUser | null> => {
     const _api = `https://api.siputzx.my.id/api/stalk/github?user=${githubUser}`;
     try {
       const res = await fetch(_api);
       const result = await res.json();
       if (!result.status) {
-      throw new Error("err");
+        throw new Error("Error fetching GitHub user data");
       }
       const {
         username,
@@ -79,12 +109,11 @@ const APIS = {
         accountCreated: new Date(created_at).toLocaleString(),
         lastUpdated: new Date(updated_at).toLocaleString(),
       };
-    } catch (error) {
+    } catch (error: any) {
       console.error(error.message);
       return null;
     }
   },
 };
 
-module.exports = { APIS };
-        
+export { APIS };
