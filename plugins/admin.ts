92894dbacaa39a16f6744ae7059f24516bafaa72
naxor <naxordeve@gmail.com>
2025-02-18 13:49:26 +0200

    naxor

---
+++ plugins/admin.ts
@@ -0,0 +1,42 @@
+import { CreatePlug } from "../lib/commands";
+
+CreatePlug({
+  command: "block",
+  category: "admin",
+  desc: "Block a user",
+  execute: async (message: any, conn: any) => {
+    if (!message.isFromMe) return;
+    const target = message.quoted
+      ? message.quoted.sender
+      : !message.isGroup
+      ? message.user
+      : null;
+    if (!target) return conn.sendMessage(message.user, { text: "Please reply to a user" });
+    await conn.updateBlockStatus(target, "block");
+    conn.sendMessage(message.user, {
+      text: `_Blocked_ @${target.split("@")[0]}`,
+      mentions: [target],
+    });
+  },
+});
+
+CreatePlug({
+  command: "unblock",
+  category: "admin",
+  desc: "Unblock a user",
+  execute: async (message: any, conn: any) => {
+    if (!message.isFromMe) return;
+    const target = message.quoted
+      ? message.quoted.sender
+      : !message.isGroup
+      ? message.user
+      : null;
+    if (!target) return conn.sendMessage(message.user, { text: "Please reply to a user" });
+    await conn.updateBlockStatus(target, "unblock");
+    conn.sendMessage(message.user, {
+      text: `_Unblocked_ @${target.split("@")[0]}`,
+      mentions: [target],
+    });
+  },
+});
+        
