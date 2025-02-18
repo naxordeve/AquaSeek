11f99dc16a7ca53b3b07535c28d5a10553548176
naxor <naxordeve@gmail.com>
2025-02-18 18:28:49 +0200

    naxor

---
+++ config.ts
@@ -0,0 +1,19 @@
+{
+  "compilerOptions": {
+    "target": "ES6",
+    "module": "commonjs",
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true,
+    "outDir": "./dist",
+    "rootDir": "./", 
+    "resolveJsonModule": true
+  },
+  "include": [
+    "lib/**/*.ts",
+    "index.ts",
+    "plugins/**/*.ts"
+  ],
+  "exclude": ["node_modules", "dist"]
+}
