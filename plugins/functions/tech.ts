191932e1ad5e55c263147f8fd752deed0ce1a488
naxor <naxordeve@gmail.com>
2025-02-19 22:15:37 +0200

    Update tech.ts

--- plugins/functions/tech.ts
+++ plugins/functions/tech.ts
@@ -1,39 +1,14 @@
-import fetch from "node-fetch";
+import axios from "axios";  
 
-interface NewsData {
-  title: string;
-  link: string;
-  image: string;
-  description: string;
-  owner: string;
-}
-
-interface NewsAPI {
-  status: boolean;
-  BK9: {
-    title: string;
-    link: string;
-    img: string;
-    desc: string;
-  };
-  owner: string;
-}
-
-async function TNewsDetails(): Promise<NewsData | null> {
-  const url = "https://bk9.fun/details/tnews";
-  const res = await fetch(url);
-  const data: NewsAPI = await res.json();
-  if (data.status) {
-    return {
-      title: data.BK9.title,
-      link: data.BK9.link,
-      image: data.BK9.img,
-      description: data.BK9.desc,
-      owner: data.owner,
-    };
-  }
-  return null;
-}
+interface NewsData { title: string; link: string; image: string; description: string; owner: string; }  
+interface NewsAPI { status: boolean; BK9: { title: string; link: string; img: string; desc: string; }; owner: string; }  
+async function TNewsDetails(): Promise<NewsData | null> {  
+  try { const { data } = await axios.get<NewsAPI>("https://bk9.fun/details/tnews");  
+    return data.status ? { title: data.BK9.title, link: data.BK9.link, image: data.BK9.img, description: data.BK9.desc, owner: data.owner } : null;  
+  } catch (error) {  
+    console.error(error);  
+    return null;  
+  }  
+}  
 
 export { TNewsDetails };
-  
