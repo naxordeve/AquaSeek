70478aeebe468376aa4dcf09d4bf18ab7995a03f
naxor <naxordeve@gmail.com>
2025-02-19 19:29:48 +0200

    Update diffuser.ts

--- plugins/functions/diffuser.ts
+++ plugins/functions/diffuser.ts
@@ -1,27 +1,20 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 interface DiffuserImg {
   imageBuffer?: ArrayBuffer;
   error?: string;
 }
-
 export async function Diffuser(prompt: string, model?: string | null): Promise<DiffuserImg> {
   const models = ['3d', 'manga', 'realistic', 'anime', 'pixel', 'lofi'];
   if (!model) {
-    model = models[Math.floor(Math.random() * models.length)];
-  }
+    model = models[Math.floor(Math.random() * models.length)];}
   if (!models.includes(model)) {
-    return { error: `Supported models: ${models.join(', ')}` };
-  }
-  const apiUrl = `https://itzpire.com/ai/stable-diffusion?prompt=${prompt}&model=${model}`;
-  try { const response = await fetch(apiUrl);
-    if (!response.ok) {
-      return { error: `HTTP Error: ${response.status}` };
-    }
-    const imageBuffer = await response.arrayBuffer();
-    return { imageBuffer };
+    return { error: `Supported models: ${models.join(', ')}` };}
+  const apiUrl = `https://itzpire.com/ai/stable-diffusion?prompt=${encodeURIComponent(prompt)}&model=${model}`;
+  try {
+    const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+    return { imageBuffer: response.data };
   } catch (error) {
     return { error: 'err' };
   }
 }
-  
