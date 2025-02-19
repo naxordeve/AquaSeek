3258eda3d9aea6b4ee15f4eea6b89b9f982b0c9b
naxor <naxordeve@gmail.com>
2025-02-19 22:17:17 +0200

    Update tiktoks.ts

--- plugins/functions/tiktoks.ts
+++ plugins/functions/tiktoks.ts
@@ -1,51 +1,18 @@
-import fetch from 'node-fetch';
+import axios from "axios";  
 
-interface Video {
-  title: string;
-  duration: number;
-  region: string;
-  videoId: string;
-  thumbnail: string;
-  createdAt: string;
-  stats: any;  
-  music: any;  
-  author: any; 
-  media: any;  
-}
-
-interface Searcher {
-  status: boolean;
-  result: Video[];
-}
-interface ErrorR {
-  error: string;}
-class TikTokS {
-  private x: string;
-  constructor(x: string) {
-    this.x = x;
-  }
-
-  async search(query: string): Promise<(Video | ErrorR)[]> {
-    const v = `${this.x}?query=${query}`;
-    const response = await fetch(v);
-    const data: Searcher = await response.json();
-    if (data.status) {
-      return data.result.map(video => ({
-        title: video.title,
-        duration: video.duration,
-        region: video.region,
-        videoId: video.videoId,
-        thumbnail: video.thumbnail,
-        createdAt: video.createdAt,
-        stats: video.stats,
-        music: video.music,
-        author: video.author,
-        media: video.media
-      }));
-    } else {
-      return [{ error: "No results found" }];
-    }
-  }
-}
+interface Video { title: string; duration: number; region: string; videoId: string; thumbnail: string; createdAt: string; stats: any; music: any; author: any; media: any; }  
+interface Searcher { status: boolean; result: Video[]; }  
+interface ErrorR { error: string; }  
+class TikTokS {  
+  private x: string;  
+  constructor(x: string) { this.x = x; }  
+  async search(query: string): Promise<(Video | ErrorR)[]> {  
+    try { const { data } = await axios.get<Searcher>(`${this.x}?query=${query}`);  
+      return data.status ? data.result : [{ error: "No results found" }];  
+    } catch (error) {  
+      return [{ error: "Request failed" }];  
+    }  
+  }  
+}  
 
 export default TikTokS;
