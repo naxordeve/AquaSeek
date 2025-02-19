19139f6d67a772aa06059128a4d1dc64858a5303
naxor <naxordeve@gmail.com>
2025-02-19 19:27:58 +0200

    Update fbdl.ts

--- plugins/functions/fbdl.ts
+++ plugins/functions/fbdl.ts
@@ -1,22 +1,20 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 const Func = async (url: string): Promise<{ "720p": string; "360p": string } | null> => {
   const _api = `https://diegoson-naxordeve.hf.space/facebook?url=${url}`;
   try {
-    const res = await fetch(_api);
-    const data = await res.json();
-
-    if (data && data.data) {
+    const { data } = await axios.get(_api);
+    if (data?.data) {
       return {
         "720p": data.data["720p (HD)"],
         "360p": data.data["360p (SD)"]
       };
-    } else {
-      return null;
     }
+    return null;
   } catch (error) {
     return null;
   }
 };
 
 export { Func };
+                                                                   
