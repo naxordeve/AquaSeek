1e15e965f449e7fd2def65bf3a5d8d9ecd7cf134
naxor <naxordeve@gmail.com>
2025-02-19 19:00:47 +0200

    Update search.ts

--- plugins/search.ts
+++ plugins/search.ts
@@ -9,7 +9,7 @@ CreatePlug({
   desc: 'Shorten a URL using TinyURL API',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
     if (!match) return void (await message.reply("Please provide a url"));
-    return void (await message.react("✅"));
+    return void (await message.react("✅");
     const tin = new TinyURL('https://api.diioffc.web.id/api/tools/tinyurl?url=');
     const msg = await tin.shortenUrl(match);
     return void (await message.reply(`*Here it Is*:\n ${msg}`));
@@ -22,7 +22,7 @@ CreatePlug({
   desc: 'Searches via bing for a given query',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
     if (!match) return void (await message.reply('You need to provide a query to search'));
-    return void (await message.react("✅"));
+    return void (await message.react("✅");
     const voidi = new BingSearch('https://apiikann.vercel.app/api/bingsrc?q=');
     const results = await voidi.Res(match);
     if (results.length === 0) return;
@@ -40,7 +40,7 @@ CreatePlug({
   execute: async (message: any, conn: any, match: string): Promise<void> => {
     const query = match;
     if (!query) return void (await message.reply('_Please provide a search query_'));
-    return void (await message.react("✅"));
+    return void (await message.react("✅");
     const wiki = await searchWikipedia(query);
     return void (await message.reply(wiki));
   }
@@ -52,7 +52,7 @@ CreatePlug({
   desc: 'Search on Pinterest by a query',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
     if (!match) return void (await message.reply('_Please provide a query to search for_'));
-    return void (await message.react("✅"));
+    return void (await message.react("✅");
     const voidi = await searchPinterest(match);
     return void (await message.reply('Loading...'));
     if (voidi.error) return void (await message.reply(`${voidi.error}`));
