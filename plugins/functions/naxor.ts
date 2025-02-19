a580c0d86f384163eb5ad3044b5b17ec7ba63866
naxor <naxordeve@gmail.com>
2025-02-19 22:27:19 +0200

    Update naxor.ts

--- plugins/functions/naxor.ts
+++ plugins/functions/naxor.ts
@@ -1,18 +1,11 @@
-import fetch from 'node-fetch';
-
-interface APIResponse<T> {
-  status: boolean;
-  data?: T;
-  message?: string;
-}
+import axios from 'axios';
 
 const API_BASE_URL = "https://api.siputzx.my.id/api/ai";
 const AI_APIS = {
   Yousearch: async (query: string): Promise<any | string> => {
     const url = `${API_BASE_URL}/yousearch?text=${query}`;
     try {
-      const res = await fetch(url);
-      const data: APIResponse<any> = await res.json();
+      const { data } = await axios.get(url);
       if (data.status) {
         return data.data;
       } else {
@@ -26,9 +19,7 @@ const AI_APIS = {
 
   Venice: async (prompt: string): Promise<string> => {
     const url = `${API_BASE_URL}/venice?prompt=${prompt}`;
-    try {
-      const res = await fetch(url);
-      const data: APIResponse<string> = await res.json();
+    try { const { data } = await axios.get(url);
       if (data.status) {
         return data.message || '';
       } else {
@@ -42,9 +33,7 @@ const AI_APIS = {
 
   Diffuser: async (prompt: string): Promise<any | string> => {
     const url = `${API_BASE_URL}/stable-diffusion?prompt=${prompt}`;
-    try {
-      const res = await fetch(url);
-      const data: APIResponse<any> = await res.json();
+    try { const { data } = await axios.get(url);
       if (data.status) {
         return data.data;
       } else {
@@ -58,9 +47,7 @@ const AI_APIS = {
 
   BlackBox: async (content: string): Promise<any | string> => {
     const url = `${API_BASE_URL}/blackboxai?content=${content}`;
-    try {
-      const res = await fetch(url);
-      const data: APIResponse<any> = await res.json();
+    try { const { data } = await axios.get(url);
       if (data.status) {
         return data.data;
       } else {
@@ -72,11 +59,9 @@ const AI_APIS = {
     }
   },
 
-  AoyoContent: async (content: string): Promise<any | string> => {
+ /* AoyoContent: async (content: string): Promise<any | string> => {
     const url = `${API_BASE_URL}/aoyo?content=${content}`;
-    try {
-      const res = await fetch(url);
-      const data: APIResponse<any> = await res.json();
+    try { const { data } = await axios.get(url);
       if (data.status) {
         return data.data;
       } else {
@@ -86,13 +71,11 @@ const AI_APIS = {
       console.error(error.message);
       return 'Try again later';
     }
-  },
+  },*/
 
   Mistral: async (content: string): Promise<any | string> => {
     const url = `${API_BASE_URL}/mistral-7b-instruct-v0.2?content=${content}`;
-    try {
-      const res = await fetch(url);
-      const data: APIResponse<any> = await res.json();
+    try { const { data } = await axios.get(url);
       if (data.status) {
         return data.data;
       } else {
