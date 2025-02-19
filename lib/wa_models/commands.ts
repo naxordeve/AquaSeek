9e12f1207c501af93ace20e89067e72f724c011c
naxor <naxordeve@gmail.com>
2025-02-19 17:56:44 +0200

    Create commands.ts

---
+++ lib/wa_models/commands.ts
@@ -0,0 +1,16 @@
+type ExecuteFunc = (message: any, conn: any, match: string) => Promise<void>;
+type Command = {
+  command: string;
+  category: string;
+  desc: string;
+  execute: ExecuteFunc; 
+  on?: string;
+};
+
+const commands: Command[] = [];
+function CreatePlug({ command, category, desc, execute, on }: Command): void {
+  const commandData: Command = { command, category, desc, execute, on };
+  commands.push(commandData);
+}
+
+export { commands, CreatePlug };
