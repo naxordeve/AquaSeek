7092669b1d7b9bd18ad14924e0ae45049ea9efd3
naxor <naxordeve@gmail.com>
2025-02-19 22:36:11 +0200

    Update Lyrics.ts

--- plugins/Lyrics.ts
+++ plugins/Lyrics.ts
@@ -1,5 +1,5 @@
-import { CreatePlug } from "../lib/index";
-import * as fetch from "node-fetch";
+import { default as CreatePlug } from "../lib/index";
+import axios from 'axios';
 
 CreatePlug({
   command: "lyrics",
@@ -8,11 +8,9 @@ CreatePlug({
   execute: async (message: any, conn: any, match: string): Promise<void> => {
     if (!match) return void (await message.reply("_Please provide a song name_"));
     await message.react("✅");
-    const response = await fetch(`https://diegoson-naxordeve.hf.space/api/lyrics?q=${match}`);
-    const data: any[] = await response.json();
-
+    const response = await axios.get(`https://diegoson-naxordeve.hf.space/api/lyrics?q=${match}`);
+    const data: any[] = response.data;,
     if (!data?.[0]) return;
-    
     const song = data[0]; 
     const Messag = `🎶 *${song.trackName}*\nby *${song.artistName}*\n\n${song.plainLyrics}`;
     return void (message.reply(Messag));
