78fadc4a4ce05790c0b5e0a2237c763cd13dd97b
naxor <naxordeve@gmail.com>
2025-02-19 12:03:36 +0200

    Update mediadl.ts

--- plugins/mediadl.ts
+++ plugins/mediadl.ts
@@ -8,8 +8,8 @@ CreatePlug({
   category: 'Artificial',
   desc: 'lepton ai',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
-    await message.react('❣️');
-    if (!match) return message.reply('Please provide a prompt');
+    if (!match) return void (await message.reply('Please provide a prompt'));
+    return void (await message.react("✅"));
     const apiKey = 'jawa';
     const lepton = new LeptonAPI(apiKey);
     const result = await lepton.getResult(match);
@@ -17,7 +17,7 @@ CreatePlug({
       const res = `*\n\n${result.answer}\n\n*Sources:*\n${result.sources
         .map((source: any, index: number) => `${index + 1}. [${source.name}](${source.url})`)
         .join('\n')}`;
-      return message.reply(res);
+      return void (await message.reply(res));
     }
   },
 });
@@ -27,8 +27,8 @@ CreatePlug({
   category: 'download',
   desc: 'Download TikTok videos',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
-    await message.react('❣️');
-    if (!match) return message.reply('_Please provide a TikTok URL_');
+    if (!match) return void (await message.reply('_Please provide a TikTok URL_'));
+    return void (await message.react("✅"));
     const videos = await tiktokdl(match).catch((error: Error) => message.reply(`${error.message}`));
     if (videos) {
       await conn.sendMessage(message.user, {
@@ -44,8 +44,8 @@ CreatePlug({
   category: 'download',
   desc: 'Download Instagram videos',
   execute: async (message: any, conn: any, match: string): Promise<void> => {
-    await message.react('❣️');
-    if (!match) return message.reply('_Please provide an Instagram video URL_');
+    if (!match) return void (await message.reply('_Please provide an Instagram video URL_'));
+    return void (await message.react("✅"));
     const result = await Func(match, 'instagram');
     if (result.platform === 'instagram' && result.mediaUrl) {
       await conn.sendMessage(message.user, {
