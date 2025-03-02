e9eda0b7facff845172f65ad7c7788e9e1ce9334
naxor <naxordeve@gmail.com>
2025-03-02 20:59:40 +0200

    Update Lyrics.ts

--- plugins/Lyrics.ts
+++ plugins/Lyrics.ts
@@ -1,4 +1,4 @@
-import { default as CreatePlug } from "../lib/index";
+import { CreatePlug, getLang } from "../lib/index";
 import axios from "axios";
 
 CreatePlug({
@@ -6,19 +6,14 @@ CreatePlug({
   category: "search",
   desc: "Fetches lyrics based on the query",
   execute: async (message: any, conn: any, match: string): Promise<void> => {
-    if (!match) {
-      await message.reply("_Please provide a song name_");
-      return;
-    }
+   const msgs = getLang();
+    if (!match) return void (await message.reply(msgs.lyrics_msg));
     await message.react("✅");
     const response = await axios.get("https://diegoson-naxordeve.hf.space/api/lyrics", {
       params: { q: match }, 
     });
     const data: any[] = response.data;
-    if (!data?.length) {
-      await message.reply("_No lyrics found_");
-      return;
-    }
+    if (!data?.length) return void (await message.reply(msgs.lyrics_sorry));
     const song = data[0];
     const messagei = `🎶 *${song.trackName}*\nby *${song.artistName}*\n\n${song.plainLyrics}`;
     await message.reply(messagei);
