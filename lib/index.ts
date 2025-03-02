2b6ad699483e15ae4dcef12820bd07c35ec70b85
naxor <naxordeve@gmail.com>
2025-03-02 20:51:59 +0200

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export * from './wa_models/commands';
 export * from './wa_models/fancy';
 export * from './wa_models/extract';
 export * from './wa_models/plugins';
+export * from './wa_models/lang';
