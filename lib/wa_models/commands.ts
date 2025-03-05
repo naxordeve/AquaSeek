a9522180675f69b1684644f3889c6944c6c7a5fc
naxor <naxordeve@gmail.com>
2025-03-05 20:57:41 +0200

    Update commands.ts

--- lib/wa_models/commands.ts
+++ lib/wa_models/commands.ts
@@ -1,16 +1,34 @@
 type ExecuteFunc = (message: any, conn: any, match: string) => Promise<void>;
 type Command = {
-  command: string;
-  category: string;
-  desc: string;
-  execute: ExecuteFunc; 
-  on?: string;
+    command: string;
+    category: string;
+    desc: string;
+    execute: ExecuteFunc;
+    on?: string;
+    permission: boolean;
 };
 
 const commands: Command[] = [];
-function CreatePlug({ command, category, desc, execute, on }: Command): void {
-  const commandData: Command = { command, category, desc, execute, on };
-  commands.push(commandData);
+function CreatePlug({ 
+    category, 
+    desc, 
+    execute, 
+    on, 
+    fromMe = false, 
+    command, 
+}: Omit<Command, 'permission' | 'command'> & { fromMe?: boolean, command: string | string[] }): void {
+    const irismd = typeof command === 'string' ? [command] : command;
+    for (const cmd of irismd) {
+        const commandData: Command = { 
+            command: cmd,
+            category, 
+            desc, 
+            execute, 
+            on, 
+            permission: fromMe 
+        };
+        commands.push(commandData);
+    }
 }
 
 export default { commands, CreatePlug };
