59cb9a64aa995424ae13ce999bd02855677d6979
naxor <naxordeve@gmail.com>
2025-03-07 23:36:38 +0200

    Update youtube.ts

--- plugins/youtube.ts
+++ plugins/youtube.ts
@@ -1,5 +1,32 @@
-import { CreatePlug, getLang } from '../lib/index';
+import { CreatePlug, getLang, monospace } from '../lib/index';
 import axios from 'axios';
+import { YouTubeSearch } from 'youtube-search-api';
+
+CreatePlug({
+    command: 'yts',
+    category: 'search',
+    desc: 'Search YouTube videos',
+    execute: async (message: any, conn: any, match: string): Promise<void> => {
+        const msgs = getLang();
+        if (!match) return void (await message.reply(msgs.query_msg));
+        const p = await YouTubeSearch.GetListByKeyword(match, false, 18);
+        if (!p.items || p.items.length === 0) return void (await message.reply(msgs.notfound));
+        let q = '乂  *Y T - S E A R C H*\n\n';
+        for (let i = 0; i < p.items.length; i++) {
+            const video = p.items[i];
+            q += `*${i + 1}.* ${monospace(video.title)}\n`;
+            q += `↳ https://www.youtube.com/watch?v=${video.id}\n`;
+        }
+
+        const vi = p.items[0];
+        await conn.sendMessage(message.user, {
+            image: { url: vi.thumbnails[0].url },
+            caption: q.trim()
+        }, {
+            quoted: message
+        });
+    },
+});
 
 CreatePlug({
     command: ['ytmp3', 'mp3'],
