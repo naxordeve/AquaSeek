e7bd374591a360b70de20742bbee982c810407e4
naxor <naxordeve@gmail.com>
2025-03-09 14:28:36 +0200

    Create welcome.ts

---
+++ lib/welcome.ts
@@ -0,0 +1,58 @@
+import { WASocket, GroupMetadata } from "@whiskeysockets/baileys";
+import { getSettings } from "./database";
+
+function monospace(text: string): string {
+  return text
+    .split("")
+    .map((char) =>
+      char >= "A" && char <= "Z"
+        ? String.fromCharCode(char.charCodeAt(0) + 0x1d670 - 65)
+        : char >= "a" && char <= "z"
+        ? String.fromCharCode(char.charCodeAt(0) + 0x1d68a - 97)
+        : char >= "0" && char <= "9"
+        ? String.fromCharCode(char.charCodeAt(0) + 0x1d7f6 - 48)
+        : char
+    )
+    .join("");
+}
+
+async function getProfilePicture(sock: WASocket, jid: string) {
+  try { return await sock.profilePictureUrl(jid, "image");
+  } catch {
+    return "https://i.imgur.com/OC9DNy3.png"; 
+  }}
+
+export async function SettingsDB(sock: WASocket, update: any) {
+  const { id, participants, action } = update;
+  const v = await getSettings(id);
+  if (!v) return; 
+  const groupMetadata: GroupMetadata = await sock.groupMetadata(id);
+  const mc = groupMetadata.participants.length;
+  for (const participant of participants) {
+    const num = participant.replace("@s.whatsapp.net", "");
+    const At = new Date().toLocaleString("en-US", { timeZone: "UTC" });
+    const _pp = await getProfilePicture(sock, participant);
+    let message = "";
+    if (action === "add") {
+      message = `${monospace("welcome")} @${num} 
+${monospace("Joined at:")} ${monospace(At)}  
+${monospace("Member count:")} ${monospace(mc.toString())}\n\n 
+\`\`\`Have great time read the group description so you can stay *WAKAWAKA*\`\`\``;
+      await sock.sendMessage(id, {
+        image: { url: _pp },
+        caption: message,
+        mentions: [participant],
+      });
+    } else if (action === "remove") {
+      message = `${monospace("Goodbye")} @${num}
+${monospace("Remaining members:")} ${monospace(mc.toString())} 
+Take care *VIRGIN*`;
+      await sock.sendMessage(id, {
+        image: { url: _pp },
+        caption: message,
+        mentions: [participant],
+      });
+    }
+  }
+         }
+                                        
